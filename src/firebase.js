3ac6937fbcab56c87cb330c07b863779b67a4c7b
Kan <kan1657@hotmail.com>
2021-10-28 21:44:17 +0700

    Finish Header

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBylpk1xWvR2L7lSdo-OnyimhONVnlG1F8",
+  authDomain: "clone-ver2.firebaseapp.com",
+  projectId: "clone-ver2",
+  storageBucket: "clone-ver2.appspot.com",
+  messagingSenderId: "904185206433",
+  appId: "1:904185206433:web:4479269a3d0cf675bc82e0",
+  measurementId: "G-06FWQVN1JQ",
+};
