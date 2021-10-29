71cc49690d0da8f2b0074718401b0c134e6be536
Kan <kan1657@hotmail.com>
2021-10-29 13:43:59 +0700

    ADD: React Router Dom

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
 import "./App.css";
 import React from "react";
-
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import Header from "./Header";
 import Home from "./Home";
 function App() {
   return (
-    <div className="app">
-      <Header />
-      <Home />
-    </div>
+    <Router>
+      <div className="app">
+        <Switch>
+          <Route path="/checkout">
+            <Header />
+            <h1>I am a Checkout</h1>
+          </Route>
+          <Route path="/">
+            <Header />
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/Product.js
@@ -0,0 +1,26 @@
+import React from "react";
+import "./Product.css";
+function Product({ title, image, price, rating }) {
+  return (
+    <div className="product">
+      <div className="product__info">
+        <p>{title}</p>
+        <p className="product_price">
+          <small>$</small>
+          <strong>{price}</strong>
+        </p>
+        <div className="product__rating">
+          {Array(rating)
+            .fill()
+            .map((_, i) => (
+              <p>⭐️</p>
+            ))}
+        </div>
+      </div>
+      <img src={image} alt="" />
+      <button className="product__button">Add to Basket</button>
+    </div>
+  );
+}
+
+export default Product;
